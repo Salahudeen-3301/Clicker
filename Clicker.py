ba565cd0aad8391e7a8a5fb95ba58aa8557dafaa
Salahudeen-3301 <swnafee1187@gmail.com>
2024-12-04 16:25:00 +0400

    Update Clicker.py

--- Clicker.py
+++ Clicker.py
@@ -4,7 +4,7 @@ from time import sleep
 repeats = -1
 time_interval = -1
 know_cords = -1
-confirm = "False"
+confirm = "n"
 coordinates = "m"
 
 
@@ -12,12 +12,12 @@ while repeats < 1:
     repeats = int(input("1. Enter the number of repeats for the Autoclicker: "))
 while time_interval < 0:
     time_interval = float(input("2. Enter the time interval between each click for the Autoclicker: "))
-while know_cords not in ["A", "B"]:
+while know_cords not in ["Y", "n"]:
     know_cords = input(
         "3. If you are aware of the co-ordinates you wish to click at - press A\nIf you do not know them, and wish to "
         "be quided through it- press B\nEnter your answer here:  ")
 
-while know_cords == "A" and confirm == "False":
+while know_cords == "Y" and confirm == "n":
     x = int(input("\nEnter the X co-ordinates for the click: "))
     y = int(input("Enter the Y co-ordinates for the click: "))
     coordinates = (x, y)
@@ -25,9 +25,9 @@ while know_cords == "A" and confirm == "False":
           ". The mouse will now move to this position, please let go of the mouse.")
     sleep(3)
     pyautogui.moveTo(coordinates)
-    confirm = input("\nAre these co-ordinates correct? (True/False): ")
+    confirm = input("\nAre these co-ordinates correct? (Y/n): ")
 
-while know_cords == "B" and confirm == "False":
+while know_cords == "n" and confirm == "n":
     print("\nYou have 10 seconds to place the mouse in the location you wish to Autoclick at")
     sleep(10)
     coordinates = pyautogui.position()
